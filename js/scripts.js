7619e1fffd85f28ef1001027b5a87038345eac41
Dean Scelza <deanscelza@gmail.com>
2016-08-19 14:31:27 -0700

    add mind blown gif to inputted numbers above 50

--- js/scripts.js
+++ js/scripts.js
@@ -6,31 +6,28 @@
 
 // ----User interface logic below-----
 
-$(document).ready(function(){
-  $("form").submit(function(event){
-      event.preventDefault();
-    $("#input").click(function(){
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    $("#input").click(function() {
       $("#list").empty();
-      
     });
-
-
-
     var inputNumber = $('#input-number').val()
-
-      for (i = 1; i <= inputNumber; i++) {
+    for (i = 1; i <= inputNumber; i++) {
       if (i % 15 === 0) {
-        $('#list').append('<li>'+"pingpong"+'</li>');}
-      else if (i % 3 === 0) {
-        $('#list').append('<li>'+"ping"+'</li>');}
-      else if (i % 5 === 0) {
-        $('#list').append('<li>'+"pong"+'</li>');}
-      else{
-        $('#list').append('<li>'+i+'</li>');}
-      };
-
-
-
-
+        $('#list').append('<li>' + "pingpong" + '</li>');
+      } else if (i % 3 === 0) {
+        $('#list').append('<li>' + "ping" + '</li>');
+      } else if (i % 5 === 0) {
+        $('#list').append('<li>' + "pong" + '</li>');
+      } else {
+        $('#list').append('<li>' + i + '</li>');
+      }
+    }
+    if (i > 50) {
+      $('#list').append(
+        '<img src="http://i.imgur.com/FNQEU7n.gif" alt="mind blown">'
+      );
+    }
   });
 });
